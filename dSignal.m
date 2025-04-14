2bd81af7b7873989ed5d181ea221e95b3e17efa9
Vladimir <106733735+Artpgch@users.noreply.github.com>
2025-04-14 21:21:08 +0700

    Update dSignal.m

--- dSignal.m
+++ dSignal.m
@@ -1,10 +1,10 @@
-function dSig = dSignal(tfield, dlength, fs, btact, dData) % функция генерации кадра десятичных прямоугольных импульсов
+function dSig = dSignal(tfield, dlength, fs, btact, dData)
     dSig = uint8(zeros(1, length(tfield)));
-    k=0;
-    for i=1:dlength
+    k = 0;
+    for i = 1:dlength
         for j=1:fs*btact
                 k=k+1;
-                dSig(k)=uint8(dData(i));
+                dSig(k) = uint8(dData(i));
         end
     end
 
