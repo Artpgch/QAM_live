2b6adf23fe70d90b1ff15c2b3282baa16a06ed81
Vladimir <106733735+Artpgch@users.noreply.github.com>
2025-04-14 21:14:14 +0700

    Update Qframe.m

--- Qframe.m
+++ Qframe.m
@@ -1,10 +1,9 @@
 function [dSig, bSig, mSig, tfield, dData, bData, btact, cNums] = Qframe(dlength, M, fs, fm)
-    [dData, bData, btact] = Dataframe(dlength, M); % обращение к функции генерации кадра данных
-    [tfield, bSig] = bSignal(dlength, fs, bData, btact); % обращение к функции генерации кадра двоичных прямоугольных импульсов
-    dSig = dSignal(tfield, dlength, fs, btact, dData); % обращение функции генерации кадра десятичных прямоугольных импульсов
-    [Amp, Phi, cNums] = mData(dData, M); % обращение к функции вычисления амплитуд и фаз модуляции
-    mSig = mSignal(tfield, dlength, fs, btact, Amp, Phi, fm); % обращение к функции генерации радиосигнала
-
+    [dData, bData, btact] = Dataframe(dlength, M);
+    [tfield, bSig] = bSignal(dlength, fs, bData, btact);
+    dSig = dSignal(tfield, dlength, fs, btact, dData);
+    [Amp, Phi, cNums] = mData(dData, M);
+    mSig = mSignal(tfield, dlength, fs, btact, Amp, Phi, fm);
 end
 
 
