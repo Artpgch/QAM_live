c7562d848aac67f6d8157fcebd50d9312754a151
Vladimir <106733735+Artpgch@users.noreply.github.com>
2025-04-14 21:07:48 +0700

    Update Dataframe.m

--- Dataframe.m
+++ Dataframe.m
@@ -1,7 +1,7 @@
 function [decData, binData, btact] = Dataframe(dlength, M)
 
-btact=log2(M); % количество бит, передаваемых за 1 такт
-decData = randi([0 M-1],dlength,1); % генерация случайных десятичних чисел от 0 до M-1 в количестве dlength
+btact=log2(M); 
+decData = randi([0 M-1],dlength,1);
 stBin=dec2bin(decData,btact);
 binData=zeros(dlength,btact);
 for p=1:dlength
